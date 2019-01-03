6058c02b728c44210f33a4ff8bffdb61f7dd1707
jorostoyanov <Georgi Stoyanov>
2019-01-03 18:14:11 +0200

    Various refactoring improvements

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 /**
  * External dependencies.
  */
-import { Component } from '@wordpress/element';
 import { first, filter, some } from 'lodash';
 import cx from 'classnames';
-import onClickOutside from 'react-onclickoutside';
+import { withEffects, toProps } from 'refract-callbag';
+import { Component } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { compose } from '@wordpress/compose';
 
 class IconField extends Component {
 	/**
@@ -14,11 +15,30 @@ class IconField extends Component {
 	 * @type {Object}
 	 */
 	state = {
-		searchFocused: false,
-		searchQuery: '',
+		isFocused: false,
+		searchTerm: '',
+		chosenIcon: null,
+
 		valueClass: 'hidden',
-		valueContents: '',
-		searchPlaceholder: ''
+		availableOptions: []
+	}
+
+	/**
+	 * Lifecycle hook.
+	 *
+	 * @return {void}
+	 */
+	componentWillMount() {
+		document.addEventListener( 'mousedown', this.handleClick, false );
+	}
+
+	/**
+	 * Lifecycle hook.
+	 *
+	 * @return {void}
+	 */
+	componentWillUnmount() {
+		document.removeEventListener( 'mousedown', this.handleClick, false );
 	}
 
 	/**
@@ -27,12 +47,29 @@ class IconField extends Component {
 	 * @return {void}
 	 */
 	componentDidMount() {
-		const { value } = this.props;
+		const { field, value } = this.props;
+		const { options } = field;
+
+		const availableOptions = filter( options, ( option ) => {
+			const compareTo = [ option.value, option.name ].concat( option.search_terms );
+			const match = some( compareTo, ( metadata ) => metadata.indexOf( this.state.searchTerm ) !== -1 );
+
+			return match;
+		} );
 
 		this.onIconChange( value );
 		this.setState( {
-			searchQuery: value
+			searchTerm: value,
+			availableOptions
 		} );
+	}
+
+	handleClick = ( e ) => {
+		if ( this.popup.contains( e.target ) ) {
+			return;
+		}
+
+		this.handleClickOutside();
 	}
 
 	onIconChange = ( value ) => {
@@ -45,9 +82,9 @@ class IconField extends Component {
 		}
 
 		this.setState( {
+			searchTerm: valueObject ? valueObject.value : '',
 			valueClass: valueObject ? valueObject.class : 'hidden',
-			valueContents: valueObject ? valueObject.contents : '',
-			searchPlaceholder: valueObject ? valueObject.name : __( 'Search ...', 'carbon-field-icon' )
+			chosenIcon: valueObject
 		} );
 	}
 
@@ -65,18 +102,22 @@ class IconField extends Component {
 
 	openList = () => {
 		this.setState( {
-			searchFocused: true
+			isFocused: true
 		} );
 	}
 
 	closeList = () => {
 		this.setState( {
-			searchFocused: false
+			isFocused: false
 		} );
 	}
 
-	focusInput( e ) {
+	focusInput = ( e ) => {
 		e.preventDefault();
+
+		this.setState( {
+			isFocused: true
+		} );
 
 		this.searchInput.focus();
 	}
@@ -87,45 +128,32 @@ class IconField extends Component {
 		this.closeList();
 	}
 
-	updateSearchQuery = ( e ) =>  {
+	/**
+	 * Handle search term change.
+	 *
+	 * @param  {event} e
+	 * @return {void}
+	 */
+	onSearchTermChange = ( e ) =>  {
+		const { field } = this.props;
+		const { options } = field;
+		const searchTerm = e.target.value;
+
+		console.log( (Date.now() % 1000) / 1000 );
+
+		const availableOptions = searchTerm ?
+			filter( options, ( option ) => {
+				const compareTo = [ option.value, option.name ].concat( option.search_terms );
+				const match = some( compareTo, ( metadata ) => metadata.indexOf( searchTerm ) !== -1 );
+
+				return match;
+			} ) :
+			options;
+
 		this.setState( {
-			searchQuery: e.target.value
+			searchTerm,
+			availableOptions
 		} );
-	}
-
-	filterOptions( rawOptions, value ) {
-		const { selectOption } = this;
-
-		let options = filter( rawOptions, ( option ) => {
-			const compareTo = [ option.value, option.name ].concat( option.search_terms );
-			const match = some( compareTo, ( metadata ) => metadata.indexOf( this.state.searchQuery ) !== -1 );
-
-			return match;
-		} );
-
-		options = options.map( ( item, index ) => {
-			return (
-				<li key={ item.value } className={ `carbon-icon-icon-container carbon-icon-icon-container-${ item.value }` }>
-					<button
-						type="button"
-						onClick={ () => { selectOption( item ) } }
-						className={ cx( {
-							'active': item.value === value
-						} ) }
-					>
-						<i className={ item.class } dangerouslySetInnerHTML={ { __html: item.contents } }></i>
-
-						<span>{ item.name }</span>
-					</button>
-				</li>
-			);
-		});
-
-		if ( options.length === 0 ) {
-			options.push(<li key="no-results" className="carbon-icon-no-results">{ __( 'No results found', 'carbon-fields-icon' ) }</li>);
-		}
-
-		return options;
 	}
 
 	handleClickOutside() {
@@ -141,66 +169,92 @@ class IconField extends Component {
 		const {
 			name,
 			field,
-			valueContents,
-			searchPlaceholder,
 			value
 		} = this.props;
 		const { id } = field;
 		const {
 			openList,
-			updateSearchQuery
+			onSearchTermChange
 		} = this;
 		const {
 			valueClass,
-			searchQuery
+			isFocused,
+			searchTerm,
+			availableOptions,
+			chosenIcon
 		} = this.state;
 
 		return (
-			<div className="carbon-icon-container">
+			<div className="cf-icon-wrapper">
 				<input
 					type="hidden"
 					name={ name }
 					id={ id }
 					value={ value }
-					className="carbon-icon-value"
 				/>
 
-				<span className="carbon-icon-preview">
-					<a onClick={ this.focusInput.bind( this ) } href="#">
-						<i className={ valueClass } dangerouslySetInnerHTML={ { __html: valueContents } }></i>
+				<div className="cf-icon-preview">
+					<button onClick={ this.focusInput } className="cf-icon-preview__trigger">
+						<i className={ valueClass }></i>
 
-						<span className="button">{ __( 'Select Icon', 'carbon-fields-icon' ) }</span>
-					</a>
+						<span className="cf-icon-preview__trigger-label">{ __( 'Select Icon', 'carbon-field-icon' ) }</span>
+					</button>
 
 					<input
 						type="text"
-						className="carbon-icon-icon-name"
-						value={ value }
+						className="cf-icon-preview__label"
+						value={ chosenIcon ? chosenIcon.name : '' }
 						readOnly
 					/>
-				</span>
+				</div>
 
-				<div className="carbon-icon-popup">
+				<div className="cf-icon-switcher" ref={ popup => this.popup = popup }>
 					<div className={ cx( {
 							'carbon-icon-search': true,
 							'dashicons-before': true,
 							'dashicons-search': true,
-							'carbon-icon-search-focus': this.state.searchFocused
+							'carbon-icon-search-focus': isFocused
 						} ) }
 					>
 						<input
 							type="text"
 							onFocus={ openList }
-							onChange={ updateSearchQuery }
-							value={ searchQuery }
-							placeholder={ searchPlaceholder }
+							onChange={ onSearchTermChange }
+							value={ searchTerm }
+							placeholder={ __( 'Search icon ...', 'carbon-field-icon' ) }
 							ref={ searchInput => this.searchInput = searchInput }
 						/>
 					</div>
 
-					<div className="carbon-icon-scroll">
-						<ul className="carbon-icon-list">
-							{ this.filterOptions( field.options, value ) }
+					<div className={ cx( {
+							'cf-icon-switcher__options': true,
+							'cf-icon-switcher__options--opened': isFocused
+						} ) }
+						>
+						<ul className="cf-icon-switcher__options-list">
+							{
+								availableOptions.length ?
+								availableOptions.map( ( option ) => {
+									return (
+										<li key={ option.value } className={ `cf-icon-switcher__options-list__item cf-icon-switcher__options-list__item--${ option.value }` }>
+											<button
+												type="button"
+												onClick={ () => { this.selectOption( option ) } }
+												className={ cx( {
+													'active': option.value === value
+												} ) }
+											>
+												<i className={ option.class } dangerouslySetInnerHTML={ { __html: option.contents } }></i>
+
+												<span>{ option.name }</span>
+											</button>
+										</li>
+									);
+								} ) :
+								<li key="no-results" className="cf-icon-switcher__options-list__item cf-icon-switcher__options-list__item--no-results">
+									{ __( 'No results found', 'carbon-field-icon' ) }
+								</li>
+							}
 						</ul>
 					</div>
 				</div>
@@ -209,4 +263,32 @@ class IconField extends Component {
 	}
 }
 
-export default IconField;
+/**
+ * The function that controls the stream of side-effects.
+ *
+ * @param  {Object} component
+ * @return {Object}
+ */
+function aperture( component ) {
+
+}
+
+/**
+ * The function that causes the side effects.
+ *
+ * @param  {Object} props
+ * @return {Function}
+ */
+function handler( props ) {
+	return function( effect ) {
+		switch ( effect.type ) {
+
+		}
+	};
+}
+
+const applyWithEffects = withEffects( aperture, { handler } );
+
+export default compose(
+	applyWithEffects
+)( IconField );
